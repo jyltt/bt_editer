1e93677185a563d4ddd11ecebbd9d89bd3b42df5
wuming <358591708@qq.com>
2016-08-12 00:15:27 +0800

    修改

--- Resources/FileRead.py
+++ Resources/FileRead.py
@@ -2,14 +2,7 @@ import os
 import xml.dom.minidom
 
 class FileRead:
-	# NodeTypeFunc = {};
 	def __init__(self, path):
-		# self.NodeTypeFunc['0'] = self.CreateSequence;
-		# self.NodeTypeFunc['1'] = self.CreateSelector;
-		# self.NodeTypeFunc['2'] = self.CreateParallel;
-		# self.NodeTypeFunc['3'] = self.CreateCondition;
-		# self.NodeTypeFunc['4'] = self.CreateAction;
-		# self.NodeTypeFunc['5'] = self.CreateDecorate;
 		self.filePath = path;
 		self.xmlTree = xml.dom.minidom.parse(path);
 		self.root = self.xmlTree.documentElement
@@ -17,7 +10,6 @@ class FileRead:
 
 	def ReadXml(self,nodeInfo):
 		nodeType = nodeInfo.getAttribute("type");
-		# return self.NodeTypeFunc[nodeType](nodeInfo);
 		return self.CreateNode(nodeInfo);
 
 	def CreateNode(self,nodeInfo):
@@ -39,98 +31,6 @@ class FileRead:
 			if child.nodeType == child.ELEMENT_NODE:
 				node["children"].append(self.ReadXml(child));
 		return node;
-
-	# def CreateSequence(self,nodeInfo):
-	# 	node = {};
-	# 	node["className"] = nodeInfo.getAttribute("class_name");
-	# 	node["abort"] = nodeInfo.getAttribute("abort");
-	# 	node["uuid"] = nodeInfo.getAttribute("uuid");
-	# 	print "className:"+node["className"]+" abort:"+node["abort"]+" uuid:"+node["uuid"];
-	# 	attrList = nodeInfo.getElementsByTagName('attr');
-	# 	node["attr"] = [];
-	# 	for child in attrList[0].getElementsByTagName("attr"):
-	# 		node["attr"].append(self.GetAttrByNode(child));
-
-	# 	children = nodeInfo.getElementsByTagName('child');
-	# 	for child in children[0].getElementsByTagName("node"):
-	# 		node["children"].append(self.ReadXml(child));
-	# 	return node;
-
-	# def CreateSelector(self,nodeInfo):
-	# 	node = {};
-	# 	node["className"] = nodeInfo.getAttribute("class_name");
-	# 	node["abort"] = nodeInfo.getAttribute("abort");
-	# 	node["uuid"] = nodeInfo.getAttribute("uuid");
-	# 	print "className:"+node["className"]+" abort:"+node["abort"]+" uuid:"+node["uuid"];
-	# 	attrList = nodeInfo.getElementsByTagName('attr');
-	# 	node["attr"] = [];
-	# 	for child in attrList[0].getElementsByTagName("attr"):
-	# 		node["attr"].append(self.GetAttrByNode(child));
-
-	# 	children = nodeInfo.getElementsByTagName('child');
-	# 	for child in children[0].getElementsByTagName("node"):
-	# 		node["children"].append(self.ReadXml(child));
-	# 	return node;
-	
-	# def CreateParallel(self,nodeInfo):
-	# 	node = {};
-	# 	node["className"] = nodeInfo.getAttribute("class_name");
-	# 	node["abort"] = nodeInfo.getAttribute("abort");
-	# 	node["uuid"] = nodeInfo.getAttribute("uuid");
-	# 	print "className:"+node["className"]+" abort:"+node["abort"]+" uuid:"+node["uuid"];
-
-	# 	attrList = nodeInfo.getElementsByTagName('attr');
-	# 	node["attr"] = [];
-	# 	for child in attrList[0].getElementsByTagName("attr"):
-	# 		node["attr"].append(self.GetAttrByNode(child));
-
-	# 	children = nodeInfo.getElementsByTagName('child');
-	# 	node["children"] = [];
-	# 	for child in children[0].getElementsByTagName("node"):
-	# 		node["children"].append(self.ReadXml(child));
-	# 	return node;
-	
-	# def CreateCondition(self,nodeInfo):
-	# 	node = {};
-	# 	node["className"] = nodeInfo.getAttribute("class_name");
-	# 	node["abort"] = nodeInfo.getAttribute("abort");
-	# 	node["uuid"] = nodeInfo.getAttribute("uuid");
-	# 	print "className:"+node["className"]+" abort:"+node["abort"]+" uuid:"+node["uuid"];
-
-	# 	attrList = nodeInfo.getElementsByTagName('attr');
-	# 	node["attr"] = [];
-	# 	for child in attrList[0].getElementsByTagName("attr"):
-	# 		node["attr"].append(self.GetAttrByNode(child));
-
-	# 	return node;
-	
-	# def CreateAction(self,nodeInfo):
-	# 	node = {};
-	# 	node["className"] = nodeInfo.getAttribute("class_name");
-	# 	node["abort"] = nodeInfo.getAttribute("abort");
-	# 	node["uuid"] = nodeInfo.getAttribute("uuid");
-	# 	print "className:"+node["className"]+" abort:"+node["abort"]+" uuid:"+node["uuid"];
-		
-	# 	attrList = nodeInfo.getElementsByTagName('attr');
-	# 	node["attr"] = [];
-	# 	for child in attrList[0].getElementsByTagName("attr"):
-	# 		node["attr"].append(self.GetAttrByNode(child));
-
-	# 	return node;
-	
-	# def CreateDecorate(self,nodeInfo):
-	# 	node = {};
-	# 	node["className"] = nodeInfo.getAttribute("class_name");
-	# 	node["abort"] = nodeInfo.getAttribute("abort");
-	# 	node["uuid"] = nodeInfo.getAttribute("uuid");
-	# 	print "className:"+node["className"]+" abort:"+node["abort"]+" uuid:"+node["uuid"];
-		
-	# 	attrList = nodeInfo.getElementsByTagName('attr');
-	# 	node["attr"] = [];
-	# 	for child in attrList[0].getElementsByTagName("attr"):
-	# 		node["attr"].append(self.GetAttrByNode(child));
-
-	# 	return node;
 
 	def GetAttrByNode(self,node):
 		attr = {};
