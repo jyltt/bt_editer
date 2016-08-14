c4a243150798c2b0e4d82f90c5d581dbbfb92ebf
jyltt <358591708@qq.com>
2016-08-14 17:51:45 +0800

    bug fix

--- Resources/createBtHead_mac.sh
+++ Resources/createBtHead_mac.sh
@@ -1,4 +1,3 @@
 cp /Users/wuming/Library/Developer/Xcode/DerivedData/bt_editor-cetqondxzgnwkkfxgleuzfzprhgw/Build/Products/Debug/bt_editor\ Mac.app/Contents/Resources/cfg/* ./cfg/
 python main.py
 cp ./bt_list.cpp ../../xyy01/Classes/config/
-pause
